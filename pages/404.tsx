b523a076827153dafd3c4fb3c39046f6b4782570
Watataku <39507718+watataku8911@users.noreply.github.com>
2023-05-01 17:32:33 +0900

    ダークモード対応

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,30 +8,36 @@ const NotFound = () => {
         <title>Not Found</title>
       </Head>
       <section className="text-center min-h-[calc(100vh_-_170px)]">
-        <h2 className="text-3xl font-bold mb-6">
-          <span className="text-red-700 tracking-[5px] text-9xl mb-3">404</span>
+        <h2 className="text-3xl font-bold mb-6 dark:text-white">
+          <span className="text-red-700 tracking-[5px] text-9xl maxsp:text-8xl mb-4">
+            404
+          </span>
           <br />
           お探しのページは見つかりませんでした。
         </h2>
-        <p className="text-2xl mb-6 maxsp:text-base">
+        <p className="text-xl mb-8 maxsp:text-base dark:text-white">
           あなたがアクセスしようとしたページは削除されたかURLが変更されているため、
           見つけることができません。
           <br />
           以下の理由が考えられます。
         </p>
 
-        <ul className="p-5 mb-5 m-auto text-left border-4 border-gray-400 w-[55%] tbpc:w-[65%] maxsp:w-[85%]">
-          <li className="list-disc maxsp:text-xs">
+        <ul className="p-5 mb-6 m-auto text-left border-4 border-gray-400 dark:border-gray-50 w-[55%] tbpc:w-[65%] maxsp:w-[85%] dark:text-white">
+          <li className="list-disc ml-5 maxsp:text-xs">
             記事がまだ公開されていない。
           </li>
-          <li className="list-disc maxsp:text-xs">
+          <li className="list-disc ml-5 maxsp:text-xs">
             アクセスしようとしたファイルが存在しない。（ファイルの設置箇所を誤っている。）
           </li>
-          <li className="list-disc maxsp:text-xs">URLが間違っている。</li>
+          <li className="list-disc ml-5 maxsp:text-xs">URLが間違っている。</li>
         </ul>
-        <Link href={"/"}>
-          <a className="block text-blue-800 ml-auto pc:w-[15%] ">TOPへ戻る</a>
-        </Link>
+        <div className="w-[90%] text-right">
+          <Link href={"/"}>
+            <a className=" text-blue-800 border-b-blue-800 dark:border-[#ff36ab] dark:text-[#ff36ab] hover:border-b ml-auto">
+              TOPへ戻る
+            </a>
+          </Link>
+        </div>
       </section>
     </>
   );
