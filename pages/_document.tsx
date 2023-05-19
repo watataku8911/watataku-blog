372c7bac6db5fcda3220ab2af7f59150e73525bd
Watataku <39507718+watataku8911@users.noreply.github.com>
2023-05-19 11:59:26 +0900

    ダークモード時のbg 変更

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -59,7 +59,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
-        <body className="bg-blue-100 dark:bg-gray-900">
+        <body className="bg-blue-100 dark:bg-gray-800">
           <Main />
           <NextScript />
         </body>
