15acade48614c68c5eb980b4b54c216d2b73b011
Watataku <39507718+watataku8911@users.noreply.github.com>
2023-05-25 11:17:54 +0900

    Update index.tsx
    条件式が間違っていました

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home: NextPage<Props> = ({ blogs, totalCount }) => {
 
       <BlogList blogs={blogs} />
 
-      {totalCount >= PER_PAGE && <Pagination totalCount={totalCount} />}
+      {totalCount >　PER_PAGE && <Pagination totalCount={totalCount} />}
     </>
   );
 };
