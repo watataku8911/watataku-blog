493ddc14c80013b0e5edb96ab05b76847f86dbe3
Watataku <39507718+watataku8911@users.noreply.github.com>
2023-06-11 15:50:12 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-const withPWA = require("next-pwa");
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-    buildExcludes: [/middleware-manifest.json$/], // 追加
-    register: true,
-    skipWaiting: true,
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
@@ -17,31 +11,9 @@ const nextConfig = withPWA({
     });
     return config;
   },
+
   images: {
     domains: ["images.microcms-assets.io"],
     disableStaticImages: true,
   },
 });
-
-module.exports = nextConfig;
-
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     buildExcludes: [/middleware-manifest.json$/], // 追加
-//     register: true,
-//     skipWaiting: true,
-//   },
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"],
-//     });
-//     return config;
-//   },
-//   images: {
-//     domains: ["images.microcms-assets.io"],
-//     disableStaticImages: true,
-//   },
-// });
